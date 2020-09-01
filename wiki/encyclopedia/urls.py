f6c6b49726ffd5b83a87d2e036b555f4582839be
Israel Hernandez Herrera <israelhhh2@gmail.com>
2020-08-31 17:06:15 -0700

    finished search , and half of newpage

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry_name>", views.entry_page, name="entry_title")
+    path("wiki/<str:entry_name>", views.entry_page, name="entry_title"),
+    path("search", views.search, name="search"),
+    path("newPage",views.new_page, name="new_page")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django import forms
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 from . import util
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={
+        'class': 'form-control form-control-lg',
+        'rows': 5
+    }))
+    entry_text = forms.CharField(label="Entry Text", widget=forms.Textarea(attrs={
+        "class": 'form-control',
+        "rows" :10, 
+        "cols" :10
+        }))
 
 
 def index(request):
@@ -17,3 +31,43 @@ def entry_page(request, entry_name):
     else: 
         return HttpResponseNotFound('<h1>404: Page Not Found :/</h1>')
 
+def new_page(request):
+    
+    if request.method == "POST":
+        form_data = NewEntryForm(request.POST)
+        if form_data.is_valid():
+            title = form_data.cleaned_data['title']
+            textarea = form_data.cleaned_data['entry_text']           
+            util.save_entry(title, textarea)
+        else:
+            return render(request, "entry.html", {
+                "form": form_data
+            })
+    return render(request, "entry.html", {
+        "form": NewEntryForm()
+    })
+
+def search(request):
+    if request.method == "GET":
+
+        search_input = request.GET.get('q')
+        get_entry = util.get_entry(search_input)
+
+        if(get_entry is not None):
+            return HttpResponseRedirect(reverse(f"wiki/{search_input}"))
+            # response = redirect(f"wiki/{search_input}")
+            # return response
+        
+        else:
+        
+            string_entries = []
+            for entry in util.list_entries():
+                if search_input.upper() in entry.upper():
+                    string_entries.append(entry)
+
+            return render(request, "encyclopedia/index.html", {
+                "search": True, 
+                "search_input": search_input,
+                "string_entries": string_entries
+
+            })
